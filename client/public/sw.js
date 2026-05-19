10d3ed52f0ded3caa111f5aa46cb1a2b7b5ff7d6
tryas <dradeev@gmail.com>
2026-05-19 07:14:20 +0300

    Add client/public/sw.js

---
+++ client/public/sw.js
@@ -0,0 +1,32 @@
+const CACHE = "pain-diary-v1";
+const STATIC = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(STATIC))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  // Не кэшируем API-запросы
+  if (e.request.url.includes("/api/")) return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
